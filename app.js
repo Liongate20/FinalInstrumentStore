57d24c6e53f73d0653f41272e9d0b7740c90b449
Liongate20 <97310152+Liongate20@users.noreply.github.com>
2022-04-29 14:26:32 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 let lists = [
-    {'id':1,'name':'GC82C', 'img':'images/image-000.png','type':'Classical Guitar', 'price':9500, 'brand':'YAMAHA' , 'color':'YAMAHA'},
-    {'id':2,'name':'PAC1611MS', 'img':'images/image-001.png', 'type':'Electric Guitar', 'price':1900, 'brand':'YAMAHA', 'color':'YAMAHA'},
-    {'id':3,'name':'1962 Jazzmaster', 'img':'images/image-002.png', 'type':'Electric Guitar', 'price':12000, 'brand':'Fender', 'color':'Fender'},
-    {'id':4,'name':'Precision Bass', 'img':'images/image-003.png', 'type':'Bass Guitar', 'price':4000, 'brand':'Fender', 'color':'Fender'},
-    {'id':5,'name':'M80', 'img':'images/image-004.png', 'type':'Acoustic Guitar', 'price':7000, 'brand':'Maton', 'color':'Maton'},
-    {'id':6,'name':'EM100C', 'img':'images/image-005.png', 'type':'Acoustic Guitar', 'price':4000, 'brand':'Maton', 'color':'Maton'},
+    {'id':1,'name':'GC82C', 'img':'image-000.png','type':'Classical Guitar', 'price':9500, 'brand':'YAMAHA' , 'color':'YAMAHA'},
+    {'id':2,'name':'PAC1611MS', 'img':'image-001.png', 'type':'Electric Guitar', 'price':1900, 'brand':'YAMAHA', 'color':'YAMAHA'},
+    {'id':3,'name':'1962 Jazzmaster', 'img':'image-002.png', 'type':'Electric Guitar', 'price':12000, 'brand':'Fender', 'color':'Fender'},
+    {'id':4,'name':'Precision Bass', 'img':'image-003.png', 'type':'Bass Guitar', 'price':4000, 'brand':'Fender', 'color':'Fender'},
+    {'id':5,'name':'M80', 'img':'image-004.png', 'type':'Acoustic Guitar', 'price':7000, 'brand':'Maton', 'color':'Maton'},
+    {'id':6,'name':'EM100C', 'img':'image-005.png', 'type':'Acoustic Guitar', 'price':4000, 'brand':'Maton', 'color':'Maton'},
 ]
 
 
